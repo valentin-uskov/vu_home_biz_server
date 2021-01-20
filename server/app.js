3dcf3d297f76a0ba56a62e7068b6e071c77ade82
Valentyn S. Uskov <just.valentine.now@gmail.com>
2021-01-20 17:19:55 +0300

    Change cors queue

--- server/app.js
+++ server/app.js
@@ -9,12 +9,14 @@ mongoose.connect(`mongodb+srv://vu_home_biz:h0Me20bU21v@cluster0.gk2m3.mongodb.n
 const app = express();
 const PORT = 3005;
 
+
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
 
-app.use(cors());
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error ${err}`));
