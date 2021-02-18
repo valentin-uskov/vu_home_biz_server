9cbffb67b44fc4e88ffc4e57840d8fc20d6f4e97
Valentyn S. Uskov <just.valentine.now@gmail.com>
2021-02-18 20:00:45 +0300

    Fix scheme id

--- models/project.js
+++ models/project.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-    id: String, /* Only for REDUX version of APP */
     name: String,
     budget: Number,
     currencyId: String
